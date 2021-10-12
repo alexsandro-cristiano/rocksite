93d8bba3e6e31ca196c1bbd12633dd2d3ab0e761
Alexsandro Cristiano <acg.alexsandro@hotmail.com>
2021-10-12 13:18:15 -0300

    correção de bug no fundo secundario

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -1,39 +1,34 @@
-const html = document.querySelector("html")
-const checkbox = document.querySelector("input[name=theme]")
+const html = document.querySelector('html')
+const checkbox = document.querySelector('input[name=theme]')
 
-const getStyle = (element, style) => 
-    window
-        .getComputedStyle(element)
-        .getPropertyValue(style)
+const getStyle = (element, style) =>
+  window.getComputedStyle(element).getPropertyValue(style)
 
 const initialColors = {
-  bodyColor: getStyle(html, "--body-color"),
-  bodyDColor: getStyle(html, "--bodyd-color"),
-  hfColor: getStyle(html, "--hf-color"),
-  titleColor: getStyle(html, "--title-color"),
-  textColor: getStyle(html, "--text-color"),
-  textLColor: getStyle(html, "--textol-color"),
+  bodyColor: getStyle(html, '--body-color'),
+  doisColor: getStyle(html, '--dois-color'),
+  hfColor: getStyle(html, '--hf-color'),
+  titleColor: getStyle(html, '--title-color'),
+  textColor: getStyle(html, '--text-color'),
+  textLColor: getStyle(html, '--textol-color')
 }
 
 const darkMode = {
-  bodyColor:"#171717" ,
-  bodyDColor: "#27292D" ,
-  hfColor:"#070707" ,
-  titleColor: "#dddddd",
-  textColor: "#bbbbbb"
+  bodyColor: '#171717',
+  doisColor: '#27292D',
+  hfColor: '#070707',
+  titleColor: '#dddddd',
+  textColor: '#bbbbbb'
 }
 
-const transformKey = key => 
-    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+const transformKey = key => '--' + key.replace(/([A-Z])/, '-$1').toLowerCase()
 
-
-const changeColors = (colors) => {
-    Object.keys(colors).map(key => 
-        html.style.setProperty(transformKey(key), colors[key]) 
-    )
+const changeColors = colors => {
+  Object.keys(colors).map(key =>
+    html.style.setProperty(transformKey(key), colors[key])
+  )
 }
 
-
-checkbox.addEventListener("change", ({target}) => {
-    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+checkbox.addEventListener('change', ({ target }) => {
+  target.checked ? changeColors(darkMode) : changeColors(initialColors)
 })
